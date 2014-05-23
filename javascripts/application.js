d62398f1addd98be760b3fc2fdb3d77e7c056711
Juan Pizarro <jpizarrom@gmail.com>
2014-05-23 00:29:46 -0400

    added multiples layer

--- javascripts/application.js
+++ javascripts/application.js
@@ -15,6 +15,8 @@ var OSM = (function() {
   function init() {
 	load_map();
     setup_search();
+    setup_styles();
+    setup_routing();
   }
 
   var directions = null;
@@ -36,6 +38,7 @@ var OSM = (function() {
       }
   function load_map() {
 		map = L.map('map',{
+			layers: [styles["OSM Foundation"]],
 			contextmenu: true,
 			contextmenuWidth: 140,
 			contextmenuItems: [{
@@ -62,20 +65,10 @@ var OSM = (function() {
 
 		var marker;
 
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+/*	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
-
-	control_routing = L.Routing.control({
-		waypoints: waypoints,
-//		geocoder: L.Control.Geocoder.nominatim(),
-		plan: L.Routing.plan(null, {
-		    waypointIcon: function(i) {
-		        return new L.Icon.Label.Default({ labelText: String.fromCharCode(65 + i) });
-		    }
-		})
-
-	}).addTo(map);
+*/
 
   }  
   
@@ -115,12 +108,48 @@ var OSM = (function() {
     // "Cirujano Guzman 25, Providencia" => "25, Cirujano Guzman, Providencia, Chile"
     return query.replace(/^(.[^0-9]*)\s([0-9]+)(.*)$/, "$2, $1$3") + ", Chile";
   }
+  function setup_routing() {
+	control_routing = L.Routing.control({
+		waypoints: waypoints,
+//		geocoder: L.Control.Geocoder.nominatim(),
+		plan: L.Routing.plan(null, {
+		    waypointIcon: function(i) {
+		        return new L.Icon.Label.Default({ labelText: String.fromCharCode(65 + i) });
+		    }
+		})
+
+	}).addTo(map);
+  }
   function reset_route() {
 //	waypoints = [];
 //	control_routing.setWaypoints(waypoints);
   }
-  var styles = [];
+  var styles = {
+	"OSM Foundation": L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18
+	}),
 
+	"OpenCycleMap": L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+	}),
+
+	"MapQuestOpen.OSM": L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpeg', {
+		attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+	subdomains: '1234'
+	}),
+
+	"Thunderforest.Transport": L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+	}),
+
+	"OpenMapSurfer.Roads": L.tileLayer('http://openmapsurfer.uni-hd.de/tiles/roads/x={x}&y={y}&z={z}', {
+		attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+	}),
+  };
+function setup_styles() {
+	L.control.layers(styles).addTo(map);
+  }
   return {
     init: init
   };
